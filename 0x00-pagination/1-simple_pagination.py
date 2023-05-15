a1ff4fdb412b5872b11ae39fd05b396fbbebe2e8
keroti <peterkeroti55@gmail.com>
2023-05-16 00:00:59 +0300

    Simple Pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,23 +1,54 @@
 #!/usr/bin/env python3
 """
-A simple helper function
+A simple pagination fun
 """
 
+from typing import List
+import csv
+import math
 from typing import List
 
 index_range = __import__('0-simple_helper_function').index_range
 
 
-def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(
+        self, page: int = 1, page_size: int = 10
+    ) -> List[List]:
+        pass
+
+
+def get_page(
+        self, page: int = 1, page_size: int = 10
+) -> List[List]:
     """
     Return the specified page of the
     dataset based on the page and page_size parameters.
     """
     assert isinstance(
-        page, int) and page > 0, "page must be a positive integer"
+        page, int
+    ) and page > 0, "page must be a positive integer"
     assert isinstance(
         page_size, int
-        ) and page_size > 0, "page_size must be a positive integer"
+    ) and page_size > 0, "page_size must be a positive integer"
 
     dataset = self.dataset()
     start, end = index_range(page, page_size)
