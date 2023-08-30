a940e83ed419f3e78bb299bee43883254144c904
keroti <peterkeroti55@gmail.com>
2023-08-30 10:03:37 +0300

    Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,24 @@
+// Import the Redis client library
+import { createClient } from "redis";
+
+// Create a new Redis client instance
+const client = createClient();
+
+// Listen for the "connect" event and print the message when it occurs
+client.on("ready", () => {
+    console.log("Redis client connected to the server");
+});
+
+// Listen for the "error" event and print the error message when it occurs
+client.on("error", (error) => {
+    console.error(
+        "Redis client not connected to the server:",
+        error.message
+    );
+});
+
+// Close the Redis client connection when the Node process terminates
+process.on("SIGINT", () => {
+    client.quit();
+    process.exit();
+});
